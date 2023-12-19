e0037777b84133b21b229a585e8432137b1893ae
pankajsharma909 <139841233+pankajsharma909@users.noreply.github.com>
2023-12-19 22:47:07 +0530

    Update index.js

--- pankaj_carsellbackend/index.js
+++ pankaj_carsellbackend/index.js
@@ -1,82 +1,91 @@
-// Import required modules
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 
-// Create an Express application
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://mrpankaj707:rzAWIBFGIrwuE4PR@cluster0.ai2ibdj.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://mrpankaj707:rzAWIBFGIrwuE4PR@cluster0.ai2ibdj.mongodb.net/', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// Create a user schema
 const userSchema = new mongoose.Schema({
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true },
 });
 
-// Create a User model
 const User = mongoose.model('User', userSchema);
 
-// Middleware to parse JSON requests
-app.use(express.json());
+// Middleware for handling errors
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Internal Server Error' });
+});
 
-// Register endpoint
-app.post('/api/register', async(req, res) => {
+app.post('/api/register', async(req, res, next) => {
     try {
         const { username, password } = req.body;
-
-        // Check if the username is already taken
         const existingUser = await User.findOne({ username });
+
         if (existingUser) {
             return res.status(400).json({ message: 'Username already taken' });
         }
 
-        // Hash the password
         const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Create a new user
         const newUser = new User({ username, password: hashedPassword });
         await newUser.save();
 
         res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
+        next(error); // Pass the error to the error handling middleware
     }
 });
 
-// Login endpoint
-app.post('/api/login', async(req, res) => {
+app.post('/api/login', async(req, res, next) => {
     try {
         const { username, password } = req.body;
-
-        // Find the user by username
         const user = await User.findOne({ username });
+
         if (!user) {
             return res.status(401).json({ message: 'Invalid username or password' });
         }
 
-        // Check if the password is correct
         const match = await bcrypt.compare(password, user.password);
         if (!match) {
             return res.status(401).json({ message: 'Invalid username or password' });
         }
 
-        // Generate a JWT token
-        const token = jwt.sign({ userId: user._id }, 'your-secret-key', { expiresIn: '1h' });
+        const token = jwt.sign({ userId: user._id }, "your-secret-key", { expiresIn: '1h' });
 
         res.status(200).json({ token });
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
+        next(error);
     }
 });
 
-// Start the server
+app.get('/api/protected', authenticateToken, (req, res) => {
+    res.json({ message: 'This is a protected route' });
+});
+
+function authenticateToken(req, res, next) {
+    const token = req.header('Authorization');
+
+    if (!token) return res.status(401).json({ message: 'Access denied' });
+
+    jwt.verify(token, "your-secret-key", (err, user) => {
+        if (err) return res.status(403).json({ message: 'Invalid token' });
+        req.user = user;
+        next();
+    });
+}
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(Server is running on port ${PORT});
 });
